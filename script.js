a1586ea8487764bdf44ceb2274e4e2b85840da8f
Alex <a.mercer118@gmail.com>
2022-02-20 17:47:37 -0700

    added API

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-$('.like, .dislike').on('click', function() {
-    event.preventDefault();
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-});
+const url = 'https://emojihub.herokuapp.com/api/all/category_food_and_drink'
+fetch(url)
+    .then((response) => {
+        return response.json();
+    }).then((json) => {
+        let results = ""
+        json.map(x => {
+            results += `<p>${x.htmlCode[0]}</p>`
+        })
+        document.getElementById("js_target").innerHTML = results
+    })

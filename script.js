b1e82d84c9fba0eeaffb77a782bc2fc9deb879c4
Alex <a.mercer118@gmail.com>
2022-02-21 11:00:18 -0700

    working emoji API

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const url = 'https://emojihub.herokuapp.com/api/all/category_food_and_drink'
-fetch(url)
-    .then((response) => {
-        return response.json();
-    }).then((json) => {
-        let results = ""
-        json.map(x => {
-            results += `<p>${x.htmlCode[0]}</p>`
+function getEmoji(category) {
+    const url = `https://emojihub.herokuapp.com/api/random/${category}`
+    console.log(url)
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        }).then((json) => {
+            let results = `<p>${json.htmlCode[0]}</p>`
+
+            document.getElementById("js_emoji").innerHTML = results
         })
-        document.getElementById("js_target").innerHTML = results
-    })
+}
+
+document.getElementById("faceEmoji").addEventListener("click", (event) => {
+    getEmoji("category_smileys_and_people")
+})
+document.getElementById("foodEmoji").addEventListener("click", (event) => {
+    getEmoji("category_food_and_drink")
+})
+document.getElementById("animalEmoji").addEventListener("click", (event) => {
+    getEmoji("category_animals_and_nature")
+})
+
+// $(function() {
+//     $('[data-toggle="popover"]').popover()
+// })
+
+// $('.popover-dismiss').popover({
+//     trigger: 'focus'
+// })
+
+// //   $(function () {
+// //     $('.example-popover').popover({
+// //       container: 'body'
+// //     })
+// //   })
